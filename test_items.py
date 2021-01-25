0aa87cc6d1e30d31d5f3af85a736d7cbfce9003f
Leyla <lylatsrl@gmail.com>
2021-01-25 18:06:03 +0300

    Test for check if these is an add to cart button

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from time import sleep
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_goods_has_add_to_basket_button(browser):
+def test_has_add_to_basket_button(browser):
     browser.get(link)
     sleep(30)
     add_to_basket = browser.find_element_by_css_selector('.btn-add-to-basket')
